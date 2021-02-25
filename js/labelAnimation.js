3bacf72c050334c0fee63690472b505243ac5365
sirriah <tereza.dvorakova.3@seznam.cz>
2021-02-25 23:26:17 +0100

    rename class price

--- js/labelAnimation.js
+++ js/labelAnimation.js
@@ -1,14 +1,14 @@
+// If somebody click on the input, add its parent the "normal" class 
 $('input').focus(function(){
     $(this).parents('.component-wrap').addClass('normal');
 });
 
+// if somabeody click on something else, but nothing was write inside input, the normal class is removed
 $('input').blur(function(){
     var inputValue = $(this).val();
     if ( inputValue == "" ) {
       $(this).parents('.component-wrap').removeClass('normal');
-    } else {
-      //$(this).addClass('filled');
-    }
+    } 
   })  
 
 
